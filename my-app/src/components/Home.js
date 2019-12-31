2a7a2ee579deb9cae5fa2422d8f1c800d30e08fa
kaushikdayalan <kaushikruth1@gmail.com>
2019-12-31 13:33:24 +0530

    integrated home page

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,10 +1,21 @@
 import React,{Component} from 'react'
 import Menu from '../components/Menu'
+import '../components/home.css'
 class Home extends Component{
     render(){
         return(
             <div> 
                 <Menu></Menu>
+ <header class="masthead">
+   <div class="container h-100">
+     <div class="row h-100 align-items-center">
+       <div class="col-12 text-center">
+         <h1 class="font-weight-light">Welcome to Jungle Walks Base Camp</h1>
+         <p class="lead">Wild is Calling !! Where are you ??</p>
+       </div>
+     </div>
+   </div>
+ </header>
             </div>
         )
     }
