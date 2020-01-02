d828bde1c5673271ea1a98e2aa949c378fa69c1e
kaushikdayalan <kaushikruth1@gmail.com>
2020-01-02 20:45:38 +0530

    bug fixs

---
+++ my-app/src/components/FooterMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {} from 'reactstrap';
+
+const FooterMenu = ()=>{
+    return(
+        <div className="container">
+            <h3 className="">HEAR FROM US</h3>
+        </div>
+    )
+}
+
+export default FooterMenu;
+
