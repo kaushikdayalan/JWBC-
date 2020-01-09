3232703c85559dbfc89eb89090acdb084780a9de
kaushikdayalan <kaushikruth1@gmail.com>
2020-01-09 21:36:00 +0530

    bug fixes

--- my-app/src/MainRouter.js
+++ my-app/src/MainRouter.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
 import AboutUs from './components/AboutUs'
-import Accommodation from './components/Accommodation'
+import Projects from './components/Projects'
 import ContactUs from './components/ContactUs'
 const MainRouter =()=>(
     <div>
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/About-us" component={AboutUs}/>
-            <Route exact path="/Projects" component={Accommodation}/>
+            <Route exact path="/Projects" component={Projects}/>
             <Route exact path="/Contact-us" component={ContactUs}/>
         </Switch>
     </div>
