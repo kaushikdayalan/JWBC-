47839fb1aef55ddbdd6ddfbf5497936c13b25861
kaushikdayalan <kaushikruth1@gmail.com>
2020-01-15 10:35:46 +0530

    bg for event page added

--- my-app/src/components/Events.js
+++ my-app/src/components/Events.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react'
+import Menu from './Menu'
+import '../components/events.css'
 
 class Events extends Component{
     render(){
+
         return(
-            <div>
-                <h1> Events page</h1>
+            <div className="container-fluid event-bg">
+                <header>
+                    <Menu/>
+                </header>
+                <div className="container">
+                    <div className="row justify-content-md-center">
+                        <div className="col-sm-12">
+                            <h1 className="heading-event text-center">One Big Screen in the
+                             Deep Forest</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
